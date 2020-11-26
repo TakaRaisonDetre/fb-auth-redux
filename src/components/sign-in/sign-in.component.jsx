07be70cf3ea8356b5bba97b13992d1c4e4151364
原貴悌 <taka_raison@genkiteinoiMac.local>
2020-11-27 05:00:48 +0900

    modify sign-in component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import CustomButton from '../custom-button/custombutton'
 
-import {signInWithGoogle} from '../../firebase/firebase.utils'
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils'
 
 class SignIn   extends Component {
 constructor(props){
@@ -13,9 +13,21 @@ this.state={
 }
 };
 
-handleSubmit =e => {
+handleSubmit = async e => {
     e.preventDefault();
-    this.setState({email: '', password: ''})
+   const {email, password} = this.state;
+   try {
+       await auth.signInWithEmailAndPassword(email, password);
+       this.setState({email:'', password:''})
+
+   } catch (error) {
+       console.log(error);
+   }
+}
+
+handleChange = e =>{
+    const {value, name } = e.target;
+    this.setState({[name]: value})
 }
 
     render() {
@@ -24,8 +36,8 @@ handleSubmit =e => {
                 <h2>I already jhave an account</h2>
             
             <form onSubmit={this.handleSubmit}> 
-                <input name ="email" value ={this.state.enail}></input>
-                <input name ="password" value ={this.state.password}></input>
+                <input name ="email" handleChange={this.handleChange} value ={this.state.enail}></input>
+                <input name ="password" handleChange={this.handleChange} value ={this.state.password}></input>
                 <button type="submit" value="Submit Form" ></button>
                 <CustomButton  onClick={signInWithGoogle}>Sign In with Google</CustomButton>
             </form>
